7e78ecda82e6d73b5093d080c6df2e5f20e118c0
Ernesto Perez <eperez@facturas.com.gt>
2023-05-21 15:37:26 -0600

    Commit Inicial

---
+++ src/main/java/com/umg/baseprojectumg/BaseprojectumgApplication.java
@@ -0,0 +1,13 @@
+package com.umg.baseprojectumg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseprojectumgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseprojectumgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/baseprojectumg/BaseprojectumgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.baseprojectumg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseprojectumgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
